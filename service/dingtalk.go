d6e44873493bfd4d0add20055e1d443bdef5bad0
pppscn <35696959@qq.com>
2021-01-05 19:52:44 +0800

    增加钉钉机器人推送

---
+++ service/dingtalk.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"github.com/blinkbean/dingtalk"
+	"github.com/unknwon/goconfig"
+	"log"
+)
+
+type Dingtalk struct {
+	conf *goconfig.ConfigFile
+}
+
+func NewDingtalk(conf *goconfig.ConfigFile) *Dingtalk {
+	return &Dingtalk{conf: conf}
+}
+
+func (this *Dingtalk) Send(title, msg string) error {
+	cli := dingtalk.InitDingTalkWithSecret(
+		this.conf.MustValue("dingtalk", "access_token", ""),
+		this.conf.MustValue("dingtalk", "secret", ""),
+	)
+	markdown := []string{
+		"### " + title,
+		"---------",
+		msg,
+	}
+	err := cli.SendMarkDownMessageBySlice(title, markdown)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("钉钉机器人推送成功")
+	}
+
+	return nil
+}
